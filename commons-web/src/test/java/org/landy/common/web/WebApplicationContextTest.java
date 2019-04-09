a081edb16fbba46d8260a3e909d6fbad550f1a91
landy8530 <landy8530@hotmail.com>
2019-04-09 23:56:48 +0800

    调用Java Web组件，如自动注入ServletContext Bean等需要加入@WebAppConfiguration注解才能进行相应的单元测试操作

--- commons-web/src/test/java/org/landy/common/web/WebApplicationContextTest.java
+++ commons-web/src/test/java/org/landy/common/web/WebApplicationContextTest.java
@@ -8,11 +8,13 @@ import org.landy.commons.datacache.conf.DataCacheConfiguration;
 import org.landy.commons.web.conf.WebApplicationContextConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class) //调用Spring单元测试类
+@WebAppConfiguration  //调用Java Web组件，如自动注入ServletContext Bean等
 @ContextConfiguration(classes = {
         WebApplicationContextConfiguration.class
-})
+}) //加载Spring配置文件
 public class WebApplicationContextTest {
 
     @Test
